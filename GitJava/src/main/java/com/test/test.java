45c831ee06349bdc8bcda76b92785e443d129a27
ezaharinov <emilian.zaharinov@gmail.com>
2017-01-24 16:55:49 +0200

    prudnq iz gashti

--- GitJava/src/main/java/com/test/test.java
+++ GitJava/src/main/java/com/test/test.java
@@ -5,7 +5,7 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Emo Java program!!!");
+		System.out.println("Hello Emo Java program!!! Commit!");
 
 	}
 
